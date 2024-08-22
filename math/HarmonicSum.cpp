21ec0e7371262546ea03c0a70b646ce8e0b479fc
劉元楷 <Yuankai_@outlook.com>
2024-08-22 22:44:08 +0800

    Update HarmonicSum.cpp

--- math/HarmonicSum.cpp
+++ math/HarmonicSum.cpp
@@ -10,4 +10,18 @@ struct Harmonic{
         int n = exp(k-gamma)+0.5;
         return n;
     }
+    // 16n
+    // n/1 + n/2 + n/3 + ... + n/n
+    //就是這東西 [20,10,6,5,4,3,2,2,2,2,1,1,1,1,1,1,1,1,1,1]
+    //這是N以下的全因數和
+    int nthHarmonicSum9(int n){
+        int inv2=qpow(2,MOD-2,MOD),ans=0;
+        for(int i=1;i<=n;){
+            int v = n/i; int j = n/v;
+            int area=(((j-i+1)%MOD)*((j+i)%MOD))%MOD*inv2%MOD; //梯形
+            ans=(ans+v*area%MOD)%MOD;
+            i=j+1;
+        }
+        return ans;
+    }
 };
