164809eb57ea3e6171b41a4fdc929e8575305e77
Yuankai <Yuankai_@outlook.com>
2024-08-27 19:18:09 +0800

    Update Int128IO.cpp
    add print function

--- basic/Int128IO.cpp
+++ basic/Int128IO.cpp
@@ -11,3 +11,12 @@ template<class type_name> inline type_name qr(type_name sample)
         ret=(ret<<1)+(ret<<3)+cur-'0',cur=getchar();
     return sgn==-1?-ret:ret;
 }
+inline void print(__int128 x){
+    if(x < 0){
+        putchar('-');
+        x = -x;
+    }
+    if(x > 9)
+        print(x / 10);
+    putchar(x % 10 + '0');
+}
