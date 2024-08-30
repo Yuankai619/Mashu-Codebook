6fa35c76b6a0014935ff5a455e746cce5fe4a48c
Part-time-Ray <wang.ray0321@gmail.com>
2024-08-30 21:04:47 +0800

    add partially ordered 3d

---
+++ SpecialQuestion/Partiallyordered3D.cpp
@@ -0,0 +1,30 @@
+// vec
+// {{a, b, c},
+//  {a, b, c},
+//     ...    
+//  {a, b, c}}
+// 貼上 BIT 模板
+map<vector<int>, int> cnt;
+int cdq(vector<vector<int>> &vec, int l, int r) {
+    if(l==r) return 0;
+    int mid = l+r>>1;
+    int ans = cdq(vec, l, mid)+cdq(vec, mid+1, r);
+    vector<vector<int>> temp;
+    for(int i=l, j=mid+1 ; i<=mid || j<=r ;) {
+        while(i<=mid && (j>r || vec[i][1] <= vec[j][1])) {bit.add(vec[i][2],cnt[vec[i]]); temp.push_back(vec[i++]);}
+        if(j<=r) {
+            temp.push_back(vec[j]);
+            ans += bit.query(vec[j][2]);
+        }
+    }
+    for(int i=l ; i<=mid ; i++) bit.add(vec[i][2],-cnt[vec[i]]);
+    for(int i=l ; i<=r ; i++) vec[i] = temp[i-l];
+    return ans;
+}
+int solve(vector<vector<int>> &vec) {
+    bit.init(2e5+5);
+    for(vector<int> v: vec) cnt[v]++;
+    sort(vec.begin(), vec.end());
+    vec.erase(unique(vec.begin(), vec.end()), vec.end());
+    return cdq(vec, 0, vec.size()-1);
+}
--- dataStructure/BIT.cpp
+++ dataStructure/BIT.cpp
@@ -1,19 +1,17 @@
 //注意值域
-#define lowbit(x) (x & -x)
 const int N = 1e5+5;
 int bit[N];
 struct BIT {
     int n;
-    void init(int n){this->n = n;}
+    void init(int _n){ n = _n;}
+    int low(int x) {return x&-x;}
     void update(int x, int val) {
-        for (; x <= n; x += lowbit(x))
-            bit[x] += val;
+        while(x<n) bit[x]+=val, x+=low(x);
     }
     int query(int x) {
         int res = 0;
-        for (; x; x -= lowbit(x))
-            res += bit[x];
+        while(x) res += bit[x], x-=low(x);
         return res;
     }
-    int query(int L, int R) { return query(R) - query(L - 1); }
-}
+    int query(int l, int r) {return query(r) - query(l - 1); }
+};
