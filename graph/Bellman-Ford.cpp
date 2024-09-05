9167011e118f06ae98bd88c7ad9a52c1cfc87db8
Yuankai619 <Yuankai_@outlook.com>
2024-09-05 21:07:58 +0800

    Update Bellman-Ford.cpp

--- graph/Bellman-Ford.cpp
+++ graph/Bellman-Ford.cpp
@@ -1,7 +1,9 @@
 //總共m條邊，鬆弛n-1次->O(nm)
 //在第n次做一次鬆弛，如果有點被鬆弛到，代表這張圖存在負環
-for(int i=0;i<m;i++){
-    if(dis[edge[i].u]+edge[i].w<dis[edge[i].v]){
-        dis[edge[i].v]=dis[edge[i].u]+edge[i].w;
+for(int j = 0; j < n-1; j++){
+    for(int i = 0; i < m; i++){ // 對於所有邊都嘗試鬆弛
+        if(dis[ edge[i].to ] > dis[ edge[i].from ] + edge[i].weight){
+            dis[ edge[i].to ] = dis[ edge[i].from ] + edge[i].weight;
+        }
     }
 }
