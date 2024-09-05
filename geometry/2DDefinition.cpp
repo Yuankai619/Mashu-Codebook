06b6b34b79d1f4f0d9b98a2f79ba49a10f0d1231
Yuankai619 <Yuankai_@outlook.com>
2024-09-05 20:49:50 +0800

    add TangantLineOf2Circle

--- geometry/2DDefinition.cpp
+++ geometry/2DDefinition.cpp
@@ -35,10 +35,11 @@ ld Length(Vec a){return sqrt(Dot(a,a));}
 int Sgn(double x){ return (x > -eps) - (x < eps); }//return 0: x==0, 1: x>0, -1: x<0
 ld Angle(Vec a,Vec b){return acos(Dot(a,b)/Length(a)/Length(b));}//弧度
 ld Degree(Vec a,Vec b){return Angle(a,b)*180/acos(-1);}//角度
-ld Ori(Pt a,Pt b,Pt c){return Cross(b-a,c-a);}//1.(a,b)X(a,c)的面積  2. a在bc左側>0 3. a在bc右側<0 4. a在bc上==0
+ld Ori(Pt a,Pt b,Pt c){return Cross(b-a,c-a);}//1.(a,b)x(a,c)的面積  2. a在bc左側>0 3. a在bc右側<0 4. a在bc上==0
 Vec Rotate(Vec a,ld rad){return Vec(a.x*cos(rad)-a.y*sin(rad),a.x*sin(rad)+a.y*cos(rad));}//逆時針旋轉,rad為弧度
 Vec Normal(Vec a){ld L=Length(a);return Vec(-a.y/L,a.x/L);}//單位法向量，確保a不是零向量    
 Vec Unit(Vec x) { return x / Length(x); } //單位向量
+Vec Perp( const Vec v ){ return { v.y , -v.x };}//垂直向量
 bool argcmp(const Pt &a, const Pt &b) { // 極角cmp: arg(a) < arg(b)
     int f = (Pt{a.y, -a.x} > Pt{} ? 1 : -1) * (a != Pt{});
     int g = (Pt{b.y, -b.x} > Pt{} ? 1 : -1) * (b != Pt{});
