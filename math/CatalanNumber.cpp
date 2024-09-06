bebe9b4fb5bdb63fa5ddf4364f75352afcc76be6
Yuankai619 <Yuankai_@outlook.com>
2024-09-06 13:30:22 +0800

    Update CatalanNumber.cpp

--- math/CatalanNumber.cpp
+++ math/CatalanNumber.cpp
@@ -1,5 +1,5 @@
+//1 1 2 5 14 42 132 429 1430 4862 16796
 // O(N)，要記得開long long 跟設定 MOD
 cat[0]=1; cat[1]=1;   
-for(ll i=1 ; i<N ; i++) {
-    cat[i] = cat[i-1]*(i*4-2)%MOD*qpow(i+1, MOD-2)%MOD;
-}
+for(ll i=1 ; i<N ; i++) 
+    cat[i] = cat[i-1]*(i*4-2)%MOD*qpow(i+1, MOD-2,MOD)%MOD;
