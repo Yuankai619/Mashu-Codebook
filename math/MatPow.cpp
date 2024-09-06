d4ad997765be8fbc16fdf54a6d3f4ba01dcdcd9b
Deerufin <100037208+deeveer@users.noreply.github.com>
2024-09-06 15:29:10 +0800

    Update MatPow.cpp

--- math/MatPow.cpp
+++ math/MatPow.cpp
@@ -1,9 +1,10 @@
+// mat t(r,c);
 struct mat{
     long long a[200][200],r,c; // resize
     mat(int _r,int _c){r=_r;c=_c;memset(a,0,sizeof(a));}
     void build(){for(int i=0;i<r;++i)a[i][i]=1;}
 };
-mat operator * (mat &x,mat &y){
+mat operator * (mat x,mat y){
     mat z(x.r,y.c);
     for(int i=0;i<x.r;++i)for(int j=0;j<x.c;++j)for(int k=0;k<y.c;++k)
         z.a[i][j]=(z.a[i][j]+x.a[i][k]*y.a[k][j]%MOD)%MOD;
