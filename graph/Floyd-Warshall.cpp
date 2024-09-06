c85bbc32ddc6c2d0761cfb9313cfdc9e296ccebf
Part-time-Ray <wang.ray0321@gmail.com>
2024-09-06 22:22:57 +0800

    fix fw

--- graph/Floyd-Warshall.cpp
+++ graph/Floyd-Warshall.cpp
@@ -1,20 +1,43 @@
-for(int k=0;k<n;++k){//O(N^3)
-    for(int i=0;i<n;++i){
-        for(int j=0;j<n;++j){
-            if(graph[i][k]!=MAX&&graph[k][j]!=MAX)//避免不連通圖
-                graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j]);
+const int inf = 1e17;
+int dis[105][105];
+int back[105][105];
+void fw() {
+    for(int i=0;i<n;i++) dis[i][i] = 0;
+    for(int k=0;k<n;++k){//O(N^3)
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n;++j){
+                if(dis[i][k]!=inf&&dis[k][j]!=inf){//避免不連通圖
+                    dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j]);
+                    if(i!=k) back[i][j] = back[i][k];
+                }
+            }
         }
     }
-}
-for(int k=0;k<n;++k){//判斷負環
-    for(int i=0;i<n;++i){
-        for(int j=0;j<n;++j){
-            if(graph[i][k]!=MAX&&graph[k][j]!=MAX&&graph[k][k]<0){//避免不連通圖&&負環
-                graph[i][j]=-MAX;
+    for(int k=0;k<n;++k){//判斷負環
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n;++j){
+                if(dis[i][k]!=inf&&dis[k][j]!=inf&&dis[k][k]<0){//避免不連通圖&&負環
+                    dis[i][j]=-inf;
+                }
             }
         }
     }
 }
+vector<int> path(int a, int b) {
+    if(dis[a][b]==inf || dis[a][b]==-inf) return {-1};
+    vector<int> ans;
+    int now = a;
+    while(now!=b) {
+        ans.push_back(now);
+        now = back[now][b];
+    }
+    ans.push_back(b);
+    return ans;
+}
+for u, v in edge:
+    back[u][v] = v
+    dis[u][v] = 1
+
 // if(graph[a][b]==-MAX)
 //     cout<<"-Infinity\n";
 // else if(graph[a][b]==MAX)
