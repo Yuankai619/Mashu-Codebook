92f00b83038c59381e0900cc319339af4dfbd617
Yuankai619 <Yuankai_@outlook.com>
2024-09-07 00:01:33 +0800

    delete blank

--- geometry/PickTheorm.cpp
+++ geometry/PickTheorm.cpp
@@ -1,8 +1,7 @@
 int area,in,on;//area:多邊形面積 in:內部格點數 on:邊界格點數
 void PickTheorm(Pt* p,int n){
     area=polygonArea(p,n);    
-    for(int i=0;i<n;++i){
+    for(int i=0;i<n;++i)
         on+=__gcd(abs((int)p[i].x-(int)p[(i+1)%n].x),abs((int)p[i].y-(int)p[(i+1)%n].y));
-    }
     in=abs(area)+1-on/2;
 }
