1a88e6437ed2c499a2f9568e89e132860151eaa6
Deerufin <100037208+deeveer@users.noreply.github.com>
2024-09-06 23:15:08 +0800

    Update HarmonicSum.cpp

--- math/HarmonicSum.cpp
+++ math/HarmonicSum.cpp
@@ -2,16 +2,13 @@ struct Harmonic{
     const double gamma = 0.5772156649;
     //求第N個調和級數
     double nthHarmonic(int n){
-        double result = log(n)+gamma;
-        return result;
+        double result = log(n)+gamma; return result;
     }
     //求項數n的Sn>k
     int findNearstN(int k){
-        int n = exp(k-gamma)+0.5;
-        return n;
+        int n = exp(k-gamma)+0.5; return n;
     }
-    // 16n
-    // n/1 + n/2 + n/3 + ... + n/n
+    // O(16N) n/1 + n/2 + n/3 + ... + n/n
     //就是這東西 [20,10,6,5,4,3,2,2,2,2,1,1,1,1,1,1,1,1,1,1]
     //這是N以下的全因數和
     int nthHarmonicSum9(int n){
