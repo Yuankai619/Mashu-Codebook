0400d39946878930516b96c61161743d64201ee2
Part-time-Ray <wang.ray0321@gmail.com>
2024-09-06 22:40:07 +0800

    fix dst2d

--- dataStructure/dinamicSegmentTree2D.cpp
+++ dataStructure/dinamicSegmentTree2D.cpp
@@ -20,25 +20,27 @@ struct segy {
     void pull(Node* node) {
         node->val = (node->l?node->l->val:0) + (node->r?node->r->val:0);
     }
-    void add(Node* cur, int l, int r, int pos, int val) {
+    void add(Node* &cur, int l, int r, int pos, int val) {
+        if(!cur) cur = new Node();
         if (l==r) {
             cur->val += val;
             return;
         }
         int mid = l+r>>1;
-        if(pos<=mid) {if(!cur->l) cur->l = new Node();add(cur->l, l, mid, pos, val);}
-        else {if(!cur->r) cur->r = new Node();add(cur->r, mid + 1, r, pos, val);}
+        if(pos<=mid) {add(cur->l, l, mid, pos, val);}
+        else {add(cur->r, mid + 1, r, pos, val);}
         pull(cur);
     }
 
     int query(Node* cur, int l, int r, int ql, int qr) {
+        if(!cur) return 0;
         if(ql<=l && r<=qr) {
             return cur->val;
         }
         int mid = l+r>>1;
         int ans = 0;
-        if(ql<=mid) {if(!cur->l) cur->l = new Node(); ans+=query(cur->l, l, mid, ql, qr);}
-        if(mid+1<=qr) {if(!cur->r) cur->r = new Node(); ans+=query(cur->r, mid+1, r, ql, qr);}
+        if(ql<=mid) {ans+=query(cur->l, l, mid, ql, qr);}
+        if(mid+1<=qr) {ans+=query(cur->r, mid+1, r, ql, qr);}
         pull(cur);
         return ans;
     }
@@ -59,22 +61,24 @@ struct segx {
     int n,m;
     segx(int _n=1e9, int _m=1e9) {n=_n; m=_m; root=new Node(m);}
     void init(int _n=1e9, int _m=1e9) {n=_n; m=_m; root=new Node(m);}
-    void add(Node* cur, int l, int r, int qx, int qy, int val) {
+    void add(Node* &cur, int l, int r, int qx, int qy, int val) {
+        if(!cur) cur = new Node(m);
         if(l!=r) {
             int mid = (l + r) / 2;
-            if(qx<=mid) {if(!cur->l) cur->l = new Node(m); add(cur->l, l, mid, qx, qy, val);}
-            else {if(!cur->r) cur->r = new Node(m); add(cur->r, mid + 1, r, qx, qy, val);}
+            if(qx<=mid) {add(cur->l, l, mid, qx, qy, val);}
+            else {add(cur->r, mid + 1, r, qx, qy, val);}
         }
         cur->tree_y->add(qy, val);
     }
     int query(Node* cur, int lx, int rx, int qlx, int qrx, int qly, int qry) {
+        if(!cur) return 0;
         if(qlx<=lx && rx<=qrx) {
             return cur->tree_y->query(qly, qry);
         }
         int mid = lx+rx>>1;
         int ans = 0;
-        if(qlx<=mid) {if(!cur->l) cur->l = new Node(m); ans+=query(cur->l, lx, mid, qlx, qrx, qly, qry);}
-        if(mid+1<=qrx) {if(!cur->r) cur->r = new Node(m); ans+=query(cur->r, mid+1, rx, qlx, qrx, qly, qry);}
+        if(qlx<=mid) {ans+=query(cur->l, lx, mid, qlx, qrx, qly, qry);}
+        if(mid+1<=qrx) {ans+=query(cur->r, mid+1, rx, qlx, qrx, qly, qry);}
         return ans;
     }
     int query(int qlx, int qly, int qrx, int qry) {
