37d3d29419a7abe62d27032877c6c37efb7b1549
Deerufin <100037208+deeveer@users.noreply.github.com>
2024-09-06 14:54:36 +0800

    Update phi.cpp

--- math/phi.cpp
+++ math/phi.cpp
@@ -1,6 +1,5 @@
 // 計算小於n的數中與n互質的有幾個
 // O(sqrtN)
-
 // a^b mod c = a^(b%phi(c) + phi(c)) mod c   : if b>=phi(c)
 //           = a^b mod c                     : if b< phi(c)
 int phi(int n){
@@ -12,4 +11,17 @@ int phi(int n){
     }   }
     if(a>1) res = res/a*(a-1);
     return res;
+}
+// 建表 最大1e7
+int phi[MXN];
+void phitable(int n){
+    phi[1]=1;
+    for(int i=2;i<=n;++i){
+        if(phi[i])continue;
+        for(int j=i;j<=n;j+=i){
+            int &pj=phi[j];
+            if(pj==0)pj=j;
+            pj=pj/i*(i-1);
+        }
+    }
 }
