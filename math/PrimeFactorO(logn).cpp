28eeafd28912a9c3eaf3eb1b0076b3719dd8578f
Deerufin <100037208+deeveer@users.noreply.github.com>
2024-09-06 23:17:03 +0800

    Update PrimeFactorO(logn).cpp

--- math/PrimeFactorO(logn).cpp
+++ math/PrimeFactorO(logn).cpp
@@ -2,9 +2,7 @@ vector<ll> ret;
 void fac(ll x){
     if(x<2)return;
     if (miller_rabin(x)) {
-      ret.push_back(x);
-      return;
-    }
+      ret.push_back(x); return; }
     ll f = pollard_rho(x);
     fac(f); fac(x/f);
 }
